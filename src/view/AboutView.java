29661717667fd1e43fc04b726105469cc006a064
nuefurth <nuefurth@gmail.com>
2024-01-05 14:30:56 +0000

    Add to git

--- src/view/AboutView.java
+++ src/view/AboutView.java
@@ -2,6 +2,7 @@ package view;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
@@ -15,9 +16,16 @@ public class AboutView extends BorderPane{
         VBox vb = new VBox();
         vb.setPadding(new Insets(20));
         Label title = new Label("Title");
-        Label text = new Label("Something");
+        TextArea textArea = new TextArea();
+        textArea.setText("With this mini Calculator project, the primary objective " +
+                "was to explore System Architecture. We utilized the Java language for coding, " +
+                "JavaFX for the interface, and CSS for styling. While there are improvements to consider, " +
+                "we can confidently say that we achieved the main goal. Let's connect and create!✨");
+
+        textArea.setWrapText(true);
+
         vb.setSpacing(20);
-        vb.getChildren().addAll(title,text);
+        vb.getChildren().addAll(title,textArea);
 
         setCenter(vb);
     }
